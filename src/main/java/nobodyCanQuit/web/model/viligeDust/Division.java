9a9365cc3d3323b7650253829d8f80ad7d9b4e04
pix4338 <pix4338@naver.com>
2020-06-30 20:04:50 +0900

    클래스 추가

---
+++ src/main/java/nobodyCanQuit/web/model/viligeDust/Division.java
@@ -0,0 +1,31 @@
+package nobodyCanQuit.web.model.viligeDust;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+@Data
+public class Division {
+
+    private String seoul;
+    private String busan;
+    private String daegu;
+    private String incheon;
+    private String gwangju;
+    
+    private String daejeon;
+    private String ulsan;
+    private String gyeonggi;
+    private String gangwon;
+    private String chungbuk;
+    
+    private String chungnam;
+    private String jeonbuk;
+    private String jeonnam;
+    private String gyeongbuk;
+    private String gyeongnam;
+    
+    private String jeju;
+    private String sejong;
+	
+}
---
+++ src/main/java/nobodyCanQuit/web/model/viligeDust/DuNameSelected.java
@@ -0,0 +1,12 @@
+package nobodyCanQuit.web.model.viligeDust;
+
+import lombok.Data;
+
+@Data
+public class DuNameSelected {
+	private String pm25;
+	private String pm10;
+	private String sidoName;
+	private String cityName;
+
+}
